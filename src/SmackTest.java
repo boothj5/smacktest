1b9bdb0d2610ad33460f7935bab082fce97b464a
James Booth <boothj5@gmail.com>
2013-03-30 02:28:56 +0000

    Now works with patched smack

--- src/SmackTest.java
+++ src/SmackTest.java
@@ -1,9 +1,7 @@
 import org.jivesoftware.smack.BOSHConfiguration;
 import org.jivesoftware.smack.BOSHConnection;
-import org.jivesoftware.smack.ConnectionListener;
+import org.jivesoftware.smack.SASLAuthentication;
 import org.jivesoftware.smack.XMPPException;
-
-
 
 public class SmackTest {
 
@@ -12,14 +10,12 @@ public class SmackTest {
      * @throws XMPPException 
      */
     public static void main(String[] args) throws XMPPException {
-        BOSHConfiguration conf = new BOSHConfiguration(false, "localhost", 5280, "/http-bind", "panesar");
+        BOSHConfiguration conf = new BOSHConfiguration(false, "localhost", 5280, "/http-bind/", "panesar");
+        SASLAuthentication.supportSASLMechanism("SIMPLE", 0);
         BOSHConnection connection = new BOSHConnection(conf);
         connection.connect();
-        System.out.println(connection.getConnectionID());
-        connection.login("bosh1@panesar", "panesar", "java");
-        ConnectionListener = 
-        connection.addConnectionListener(connectionListener)
-        System.out.println(connection.getConnectionID());
+        connection.login(args[0], args[1]);
+//        connection.disconnect();
     }
 
 }
